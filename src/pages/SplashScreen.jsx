07a00ee93fafa6ec98bcfd3a90d621773f31eadd
Mihail Nica <mihail.nica@gosocialdev.eu>
2023-11-19 17:45:50 +0200

    update

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -3,13 +3,7 @@ import Spinner from "../components/Spinner";
 
 
 export default function SplashScreen() {
-    return (<div style={{
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }}>
+    return (<div style='height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 4;'>
         <div class='flex flex-col justify-center items-center gap-4'>
             <h1 class='text-2xl font-bold'>Livrari Regie</h1>
             <Spinner />
